a6b09bd3b7bf4cb5218e3bd338bca6b0feccabe8
Gustavo <gfontalva@gmail.com>
2024-11-16 18:29:58 -0300

    Changed database name

--- permissions.sql
+++ permissions.sql
@@ -6,39 +6,39 @@ CREATE ROLE role_estoquista;
 CREATE ROLE role_financeiro;
 
 -- ADMIN
-GRANT ALL PRIVILEGES ON trabalhosql.* TO role_administrador;
+GRANT ALL PRIVILEGES ON localhost.* TO role_administrador;
 
 -- GERENTE
-GRANT SELECT, INSERT, UPDATE, DELETE ON trabalhosql.pedido TO role_gerente;
-GRANT SELECT, INSERT, UPDATE, DELETE ON trabalhosql.venda TO role_gerente;
-GRANT SELECT, INSERT, UPDATE ON trabalhosql.produto TO role_gerente;
-GRANT SELECT ON trabalhosql.vw_estoque_atual TO role_gerente;
-GRANT SELECT ON trabalhosql.vw_vendas_por_periodo TO role_gerente;
+GRANT SELECT, INSERT, UPDATE, DELETE ON localhost.pedido TO role_gerente;
+GRANT SELECT, INSERT, UPDATE, DELETE ON localhost.venda TO role_gerente;
+GRANT SELECT, INSERT, UPDATE ON localhost.produto TO role_gerente;
+GRANT SELECT ON localhost.vw_estoque_atual TO role_gerente;
+GRANT SELECT ON localhost.vw_vendas_por_periodo TO role_gerente;
 
 -- VENDEDOR
-GRANT SELECT, INSERT ON trabalhosql.venda TO role_vendedor;
-GRANT SELECT ON trabalhosql.produto TO role_vendedor;
-GRANT SELECT ON trabalhosql.vw_estoque_atual TO role_vendedor;
+GRANT SELECT, INSERT ON localhost.venda TO role_vendedor;
+GRANT SELECT ON localhost.produto TO role_vendedor;
+GRANT SELECT ON localhost.vw_estoque_atual TO role_vendedor;
 
 -- ESTOQUISTA
-GRANT SELECT, INSERT, UPDATE ON trabalhosql.produto TO role_estoquista;
-GRANT SELECT, INSERT ON trabalhosql.movimentacao TO role_estoquista;
-GRANT SELECT ON trabalhosql.vw_estoque_atual TO role_estoquista;
+GRANT SELECT, INSERT, UPDATE ON localhost.produto TO role_estoquista;
+GRANT SELECT, INSERT ON localhost.movimentacao TO role_estoquista;
+GRANT SELECT ON localhost.vw_estoque_atual TO role_estoquista;
 
 -- FINANCEIRO
-GRANT SELECT ON trabalhosql.venda TO role_financeiro;
-GRANT SELECT ON trabalhosql.notafiscal TO role_financeiro;
-GRANT SELECT ON trabalhosql.vw_vendas_por_periodo TO role_financeiro;
+GRANT SELECT ON localhost.venda TO role_financeiro;
+GRANT SELECT ON localhost.notafiscal TO role_financeiro;
+GRANT SELECT ON localhost.vw_vendas_por_periodo TO role_financeiro;
 
 -- USERS
-CREATE USER 'admin_user'@'%' IDENTIFIED BY 'senha_adm';
-GRANT role_administrador TO 'admin_user'@'%';
+CREATE USER 'admin_user'@'localhost' IDENTIFIED BY 'senha_adm';
+GRANT role_administrador TO 'admin_user'@'localhost';
 
-CREATE USER 'gerente_user'@'%' IDENTIFIED BY 'senha_manager';
-GRANT role_gerente TO 'gerente_user'@'%';
+CREATE USER 'gerente_user'@'localhost' IDENTIFIED BY 'senha_manager';
+GRANT role_gerente TO 'gerente_user'@'localhost';
 
 CREATE USER 'vendedor_user'@'%' IDENTIFIED BY 'senha_vendedor';
-GRANT role_vendedor TO 'vendedor_user'@'%';
+GRANT role_vendedor TO 'vendedor_user'@'localhost';
 
-CREATE USER 'estoquista_user'@'%' IDENTIFIED BY 'senha_estoquista';
-GRANT role_estoquista TO 'estoquista_user';
+CREATE USER 'estoquista_user'@'localhost' IDENTIFIED BY 'senha_estoquista';
+GRANT role_estoquista TO 'estoquista_user'@'localhost';
